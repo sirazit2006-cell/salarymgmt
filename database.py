85a89b9515cdd9d0574bcb4b8aadbed0533554ea
Siraz <sirazapp@gmail.com>
2026-03-24 11:15:14 +0530

    in-memory SQLite (StaticPool) Implemetation

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
 DATABASE_URL = "sqlite:///./employees.db"
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args={"check_same_thread": False},poolclass=StaticPool
 )
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
